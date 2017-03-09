2dd97666181fc5ec42f5865724da9701a2cbe830
Alex Podworny <alex.podworny@hotmail.com>
2017-03-09 16:32:18 -0600

    Now accessing all data, need database?

--- app/controllers/heroes.js
+++ app/controllers/heroes.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    init: function() {
+        var that = this;
+        
+        Ember.run.next(function(){
+            var heroes = that.get('model').forEach(function(hero) {
+
+                console.log(hero.get('name'));
+                hero.get('attributesByLevel').forEach(function(stat){
+                    console.log(stat['AttackSpeedRating']);
+                });
+            });
+        });
+    }
 });
