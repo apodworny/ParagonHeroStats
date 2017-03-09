6528ffd0accfc451a42a89123ef7827679b18932
Alex Podworny <alex.podworny@hotmail.com>
2017-03-09 17:05:59 -0600

    Having issues with adding more values to object

--- app/controllers/heroes.js
+++ app/controllers/heroes.js
@@ -3,14 +3,24 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     init: function() {
         var that = this;
-        
-        Ember.run.next(function(){
-            var heroes = that.get('model').forEach(function(hero) {
 
+        var heroAttackSpeeds = [{
+            name: '',
+            attackSpeed: ''
+        }];
+
+        Ember.run.next(function(){
+            var heroes = that.get('model').forEach(function(hero, index, self) {
+                //var index = this.indexOf(hero);
+                debugger
+                heroAttackSpeeds[index].name = hero.get('name');
+                console.log(heroAttackSpeeds[index].name);
                 console.log(hero.get('name'));
-                hero.get('attributesByLevel').forEach(function(stat){
-                    console.log(stat['AttackSpeedRating']);
-                });
+
+                /*hero.get('attributesByLevel').forEach(function(stat){
+                    heroAttackSpeeds[this.indexOf(hero)].attackSpeed = stat['AttackSpeedRating'];
+                    console.log(heroAttackSpeeds[this.indexOf(hero)]);
+                });*/
             });
         });
     }
